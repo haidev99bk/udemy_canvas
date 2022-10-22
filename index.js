75641ce064d8d53cae4fa332b239a53b9a6bbff3
haidev99bk <haidev99bk@gmail.com>
2022-10-22 18:02:19 +0700

    draw rectangle

---
+++ index.js
@@ -0,0 +1,10 @@
+var canvas1 = document.getElementById("section1");
+var context1 = canvas1.getContext("2d");
+
+// Blue rectangle:
+context1.fillStyle = "blue";
+context1.fillRect(0, 0, 30, 80);
+
+// Yellow rectangle:
+context1.fillStyle = "yellow";
+context1.fillRect(50, 20, 30, 80);
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://fpt-software.udemy.com/course/html5-canvas-ultimate-guide/learn/lecture/7501770#overview
